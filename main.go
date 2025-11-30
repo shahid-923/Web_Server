22d2688e350cc281aa4283254251d99736589896
Shahid <sa160030@gmail.com>
2025-11-30 19:57:27 +0530

    Updated form handler and HTML structure

--- main.go
+++ main.go
@@ -16,26 +16,35 @@ func helloHandler(w http.ResponseWriter, r *http.Request){
     }    
 	fmt.Fprintf(w, "Hello!")
 }
+func formHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method != "POST" {
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+        return
+    }
 
-func formHandler(w http.ResponseWriter, r *http.Request){
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v", err)
-		return
-	}
-    fmt.Printf("POST request successful\n") 
-	name := r.FormValue("name")
-	address := r.FormValue("address")
-	fmt.Fprintf(w, "Name = %s\n", name)
-	fmt.Fprintf(w, "Address = %s\n", address) 
+    if err := r.ParseForm(); err != nil {
+        fmt.Fprintf(w, "ParseForm() err: %v", err)
+        return
+    }
+
+    // Print to browser
+    fmt.Fprintf(w, "POST request successful\n\n")
+
+    name := r.FormValue("name")
+    address := r.FormValue("address")
+
+    fmt.Fprintf(w, "Name = %s\n", name)
+    fmt.Fprintf(w, "Address = %s\n", address)
 }
 
+
 func main(){
-	fileServer := http.FileServer(http.Dir("./static"))	// handles serving static files from the "static" directory
-	http.Handle("/", fileServer)	                        // route all requests to the file server
-	http.HandleFunc("/form", formHandler)                  // handle form submissions at "/form" endpoint
-	http.HandleFunc("/hello", helloHandler)                // handle requests at "/hello" endpoint
+	fileServer := http.FileServer(http.Dir("./static"))  	     // handles serving static files from the "static" directory
+	http.Handle("/", fileServer)	                             // route all requests to the file server
+	http.HandleFunc("/form", formHandler)                        // handle form submissions at "/form" endpoint
+	http.HandleFunc("/hello", helloHandler)                      // handle requests at "/hello" endpoint
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080",nil); err != nil {
 		log.Fatal(err)
-	} 
+	}  
 }
